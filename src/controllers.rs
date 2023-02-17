49db6e0db16af75882f74c35051150b6041df94a
Nathan Kolpa <nathan@kolpa.me>
2023-02-17 20:19:27 +0100

    basic library structure

---
+++ src/controllers.rs
@@ -0,0 +1,3 @@
+#[cfg(feature = "atmega328p")]
+#[doc(cfg(feature = "atmega328p"))]
+pub mod atmega328p;
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
 #![no_std]
+#![feature(doc_cfg)]
+
+pub mod controllers;
+
+#[cfg(feature = "hal")]
+#[doc(cfg(feature = "hal"))]
+pub mod hal;
