7fe36e58fad981bf74b6d2c1d389942534261560
Nathan Kolpa <nathan@kolpa.me>
2023-02-20 13:21:39 +0100

    move the atmega controller one module up

--- src/controllers.rs
+++ src/controllers.rs
@@ -1,5 +1,8 @@
 //! The module containing all code specific to each microcontroller.
 
 #[cfg(any(feature = "atmega328p", doc))]
+mod atmega328p;
+
+#[cfg(any(feature = "atmega328p", doc))]
 #[doc(cfg(feature = "atmega328p"))]
-pub mod atmega328p;
+pub use atmega328p::*;
